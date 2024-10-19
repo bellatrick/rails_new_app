5e109b035418727d9d41795aaac0ac782369aa5a
bellatrick <busayosamuel2016@gmail.com>
2024-10-19 13:09:40 +0100

    added 3 static pages

--- app/controllers/static_site_controller.rb
+++ app/controllers/static_site_controller.rb
@@ -4,4 +4,7 @@ class StaticSiteController < ApplicationController
 
   def help
   end
+
+  def about
+  end
 end
--- test/controllers/static_site_controller_test.rb
+++ test/controllers/static_site_controller_test.rb
@@ -4,10 +4,19 @@ class StaticSiteControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_site_home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on rails application"
   end
 
   test "should get help" do
     get static_site_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on rails application"
+  end
+
+
+  test "should get about" do
+    get static_site_about_url
+    assert_response :success
+    assert_select "title", "About | Ruby on rails application"
   end
 end
